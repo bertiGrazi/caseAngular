4a7ffc52a370abb1da36be8f9aa865c71b9cecea
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-06-05 13:49:26 -0300

    add get item list endpoint

--- case_angular/src/app/app.component.ts
+++ case_angular/src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { ItemServices } from './services/item.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'case_angular';
 
-  constructor(private _dialog: MatDialog) {}
+  constructor(private _dialog: MatDialog, private _itemService: ItemServices) {}
+
+  ngOnInit(): void {
+    this.getItemsList()
+  }
 
   openAddEditEmpForm() {
     this._dialog.open(EmpAddEditComponent);
+  }
+
+  getItemsList() {
+    this._itemService.getItemList().subscribe({
+      next: (res) => {
+        console.log(res)
+      }, 
+      error: console.log
+    })
   }
 }
