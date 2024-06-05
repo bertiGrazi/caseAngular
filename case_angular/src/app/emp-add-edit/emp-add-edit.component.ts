8dbb52e5a0c8cd9f02d41465db7597c776fa894c
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-06-05 13:04:38 -0300

    set form control name

--- case_angular/src/app/emp-add-edit/emp-add-edit.component.ts
+++ case_angular/src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-add-edit.component.scss']
 })
 export class EmpAddEditComponent {
+  empForm!: FormGroup;
+
   numbers: number[] = Array.from({ length: 100 }, (_, i) => i + 1);
+
+  constructor(private _fb: FormBuilder) {
+    this.empForm = this._fb.group({
+      nameItem: '',
+      measure: '',
+      amount: '',
+      price: '',
+      product: '',
+      validity: '',
+      fabrication: '',
+    })
+  }
 }
